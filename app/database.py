f50df9ae80e795e2942ff87512e93fca47c2c052
Pipeloluwa <91763066+Pipeloluwa@users.noreply.github.com>
2024-05-20 13:09:51 +0100

    Update database.py

--- app/database.py
+++ app/database.py
@@ -14,7 +14,7 @@ POSTGRES_DB : str = os.getenv("POSTGRES_DB")
 
 POSTGRES_URL : str = os.getenv("POSTGRES_URL")
 
-DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+DATABASE_URL = POSTGRES_URL
 engine= create_engine(DATABASE_URL)
 SessionLocal= sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
