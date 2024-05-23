cb8869ec00e403336315d94248c1234c9999d878
Pipeloluwa <91763066+Pipeloluwa@users.noreply.github.com>
2024-05-23 22:05:59 +0100

    Update database.py

--- app/database.py
+++ app/database.py
@@ -14,8 +14,8 @@ POSTGRES_DB : str = os.getenv("POSTGRES_DB")
 
 POSTGRES_URL : str = os.getenv("POSTGRES_URL")
 
-DATABASE_URL = POSTGRES_URL
-engine= create_engine(DATABASE_URL)
+DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+engine= create_engine(DATABASE_URL, pool_pre_ping= True, pool_recycle= 300) #Pre ping filters out dead connections that were hitting my system and causinh havoc when hosted
 SessionLocal= sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 Base= declarative_base()
