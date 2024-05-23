6aa612d96967f00de1cf0041dcc096a0bdabff4e
Pipeloluwa <91763066+Pipeloluwa@users.noreply.github.com>
2024-05-23 22:27:27 +0100

    Update database.py

--- app/database.py
+++ app/database.py
@@ -6,15 +6,10 @@ import os
 
 load_dotenv()
 
-POSTGRES_USER : str = os.getenv("POSTGRES_USER")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-POSTGRES_SERVER : str = os.getenv("POSTGRES_SERVER","localhost")
-POSTGRES_PORT : str = os.getenv("POSTGRES_PORT")
-POSTGRES_DB : str = os.getenv("POSTGRES_DB")
 
 POSTGRES_URL : str = os.getenv("POSTGRES_URL")
 
-DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+DATABASE_URL = POSTGRES_URL
 engine= create_engine(DATABASE_URL, pool_pre_ping= True, pool_recycle= 300) #Pre ping filters out dead connections that were hitting my system and causinh havoc when hosted
 SessionLocal= sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
